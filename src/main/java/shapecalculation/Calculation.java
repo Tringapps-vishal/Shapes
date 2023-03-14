a26cca795bedbb9d8fdaf0e5581bbfcb1d30a4b5
Tringapps-vishal <vishal.k@tringapps.com>
2023-03-14 11:44:33 +0530

    Initial commit

---
+++ src/main/java/shapecalculation/Calculation.java
@@ -0,0 +1,50 @@
+package shapecalculation;
+
+import java.util.logging.Logger;
+public class Calculation{
+    private final Logger log = Logger.getLogger("calculation");
+    protected double base,side,length,width,radius;
+    protected float height;
+    public Calculation(double base, float height, double side)
+    {
+        this.base=base;
+        this.height=height;
+        this.side=side;
+        log.info("Triangle created successfully.");
+    }
+    public Calculation(double length, double width)
+    {
+        this.length=length;
+        this.width=width;
+        log.info("Rectangle created successfully.");
+    }
+    public Calculation(double radius)
+    {
+        this.radius=radius;
+        log.info("Circle created successfully.");
+    }
+    public double perimeter(double base, double height, double side)
+    {
+        return (base +height+side);
+    }
+    public double perimeter(double length, double width)
+    {
+        return (2*(length+width));
+    }
+    public double perimeter(double radius)
+    {
+        return (2*3.14115*radius);
+    }
+    public double area(double base, float height)
+    {
+        return (0.5 *base *height);
+    }
+    public double area(double length, double width)
+    {
+        return (length*width);
+    }
+    public double area(double radius)
+    {
+        return (3.14115* radius * radius);
+    }
+}
---
+++ src/main/java/shapemain/Main.java
@@ -0,0 +1,53 @@
+package shapemain;
+
+import shapecalculation.Calculation;
+import java.util.Scanner;
+import java.util.logging.Logger;
+public class Main {
+    private static final Logger log=Logger.getLogger("Main");
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(true)
+        {
+            log.info("1.Triangle        2.Rectangle          3.Circle           4.Exit");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    log.info("VALUES FOR TRIANGLE");
+                    log.info("Enter the base value:");
+                    double base = sc.nextInt();
+                    log.info("Enter the height value:");
+                    float height = sc.nextInt();
+                    log.info("Enter the side value:");
+                    double side = sc.nextInt();
+                    Calculation t = new Calculation(base, height, side);
+                    log.info("Perimeter of Triangle :" + t.perimeter(base, height, side));
+                    log.info("Area of Triangle :" + t.area(base,height));
+                }
+                case 2 -> {
+                    log.info("VALUES FOR RECTANGLE");
+                    log.info("Enter the length value:");
+                    double length = sc.nextInt();
+                    log.info("Enter the width value:");
+                    double width = sc.nextInt();
+                    Calculation r = new Calculation(length, width);
+                    log.info("Perimeter of Rectangle :" + r.perimeter(length, width));
+                    log.info("Area of Rectangle :" + r.area(length, width));
+                }
+                case 3 -> {
+                    log.info("VALUES FOR CIRCLE");
+                    log.info("Enter the radius value:");
+                    double radius = sc.nextInt();
+                    Calculation c = new Calculation(radius);
+                    log.info("Perimeter of Circle :" + c.perimeter(radius));
+                    log.info("Area of Circle :" + c.area(radius));
+                }
+                case 4 ->
+                    System.exit(0);
+                default ->
+                    log.info("INVALID CHOICE!!!");
+            }
+        }
+    }
+}
