ce50db3bd9383f2c9dd26073e3b48dc8723571e1
Tringapps-vishal <vishal.k@tringapps.com>
2023-03-14 13:01:51 +0530

    commit

--- src/main/java/shapecalculation/Calculation.java
+++ src/main/java/shapecalculation/Calculation.java
@@ -1,11 +1,3 @@
 package shapecalculation;
-
-import java.util.logging.Logger;
 public class Calculation{
-    private final Logger log = Logger.getLogger("calculation");
-    protected double base,side,length,width,radius;
-    protected float height;
-
-
-
 }
